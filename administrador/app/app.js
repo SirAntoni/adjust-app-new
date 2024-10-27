855eb3473d23ddc2e603d730accc3a7c61ee7630
Antony Alfredo Culqui Carranza <antonyalfredoculquicarranza@MacBook-Pro-de-Antony.local>
2024-10-27 14:13:54 -0500

    loader video

--- administrador/app/app.js
+++ administrador/app/app.js
@@ -614,7 +614,7 @@ var guardar_logo = function() {
 }
 
 var guardar_nosotrosImg = function() {
-  Notiflix.Block.Pulse('.nosotros-content');
+  
     $("#formNosotrosImg").submit(function(e) {
         e.preventDefault();
         const formData = new FormData($('#formNosotrosImg')[0]);
@@ -627,7 +627,7 @@ var guardar_nosotrosImg = function() {
             cache: false,
             processData: false,
             beforeSend: function() {
-                Notiflix.Block.Pulse('.nosotros-content');
+              Notiflix.Block.Pulse('.nosotros-content');
             },
             complete: function() {
                 Notiflix.Block.Remove('.nosotros-content');
