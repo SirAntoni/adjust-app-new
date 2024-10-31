eae0f0ab3c3058da83d5d73acb97025ab93107a7
Antony Alfredo Culqui Carranza <antonyalfredoculquicarranza@MacBook-Pro-de-Antony.local>
2024-10-31 13:02:28 -0500

    soporte mov 2

--- js/app.js
+++ js/app.js
@@ -201,8 +201,9 @@ const cargar_web = function () {
                 let formato = data.nosotrosImg.split(".");
                 $("#webNosotrosImg").html(`<img class="img-fluid mb-3 mb-lg-0" src="assets/img/${data.nosotrosImg}"
                 alt="..." />`);
-                if(formato[1] == "mp4" || formato[1] == "mov")  $("#webNosotrosImg").html(`<video width="817" height="600" controls>
+                if(formato[1] == "mp4" || formato[1] == "MOV")  $("#webNosotrosImg").html(`<video width="817" height="600" controls>
                   <source src="assets/img/${data.nosotrosImg}" type="video/mp4">
+                  <source src="assets/img/${data.nosotrosImg}" type="video/mov">
                   Your browser does not support the video tag.
                 </video>`);
                 $("#webMisionImg").html(`<img class="img-fluid" src="assets/img/${data.misionImg}" alt="..." />`);
