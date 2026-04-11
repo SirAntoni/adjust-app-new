851a34603864ddb51ba91ba76b5e8cb5b51735e1
Antony Culqui <a.culqui02@gmail.com>
2026-04-11 00:13:07 -0500

    fix config

--- administrador/config/conexion.php
+++ administrador/config/conexion.php
@@ -8,7 +8,7 @@ class Conectar
         try {
 
             if($_SERVER['SERVER_NAME'] == "v1.adjustapp.store"){
-                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=adjust_v1","adjust_v1","cO?a-lDz7044pbP/l#$c");
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=adjust_v1","adjust_v1",'cO?a-lDz7044pbP/l#$c');
             }else{
                 $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=adjust","root","123456");
             }
